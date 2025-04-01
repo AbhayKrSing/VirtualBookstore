e5ecee42e3d862722eff6990f9118e3d8f617f8c
unknown <kabhay849@gmail.com>
2025-04-01 11:30:49 +0530

    just an commit to switch to branch

--- src/main/java/com/Novel/VirtualBookStore/RepositoryInterface/CartsRepository.java
+++ src/main/java/com/Novel/VirtualBookStore/RepositoryInterface/CartsRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.Novel.VirtualBookStore.entity.Carts;
-import com.Novel.VirtualBookStore.entity.User;
 
 @Repository
 public interface CartsRepository extends JpaRepository<Carts, UUID> {
