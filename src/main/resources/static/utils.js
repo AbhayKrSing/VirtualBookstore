e5fa95c378aa072649cdcba161a656e6bcbee7be
unknown <kabhay849@gmail.com>
2025-04-08 18:40:57 +0530

    crud using derivative and routing

--- src/main/resources/static/utils.js
+++ src/main/resources/static/utils.js
@@ -1,9 +1,55 @@
-const app=angular.module("playWithAngularJS",[]);
+const app=angular.module("playWithAngularJS",["ngRoute"]);
+
+
+app.config(function($routeProvider) {
+  $routeProvider
+  .when("/users", {
+    template : `<div class="grid-container" >
+	 <user-card ng-repeat="user in Users" user="user" on-delete="deleteUser(user)" on-edit="editUser(user)">
+	 </user-card>
+	</div>`
+  })
+   .when("/adduser",{
+	templateUrl:"./templates/add-user.html"
+   })
+   .when('/edituser/:userId', {
+      templateUrl: './templates/edit-user.html',
+    })
+    .otherwise({
+      redirectTo: '/users'
+    });
+});
+
+
+app.directive("userCard",[function(){
+	return {
+		restrict:'E',
+		scope:{
+			user:'=',
+			onDelete:'&',
+			onEdit:'&'
+		},
+		replace:true,
+		templateUrl:'templates/user-card.html'
+	}
+}])
+
+app.directive("userForm",[function(){
+	return{
+    restrict:'E',		
+	scope:{
+		formData:'=',
+		onSubmit:'&'
+	},
+	replace:true,
+	templateUrl:"templates/user-form.html"
+	}
+}])
 
 
 
 
-app.controller("Mycontroller", function($rootScope,$scope,$http){
+app.controller("Mycontroller", function($rootScope,$scope,$http,$location){
      $scope.formData={
 		username:"",
 		email:"",
@@ -19,6 +65,7 @@ app.controller("Mycontroller", function($rootScope,$scope,$http){
 	$http.post("http://localhost:8080/api/user",$scope.formData)
 	.then((res)=>{
 		$scope.Users.push(res.data);
+		$location.path('/users');
 	}).catch((err)=>{
 		console.log("Error occured :"+ err.message);
 	})		
@@ -27,6 +74,8 @@ app.controller("Mycontroller", function($rootScope,$scope,$http){
 	$scope.editUser=function(user){
 	   $scope.editingUser=true;
 	   $scope.currentEditUser=angular.copy(user);
+	   $location.path('/edituser/' + user.id);
+	   
 	}
 	
 	$scope.updateUser=function(){
@@ -35,6 +84,7 @@ app.controller("Mycontroller", function($rootScope,$scope,$http){
 		$scope.UserBody.email=$scope.currentEditUser.email
 		$scope.UserBody.password=$scope.currentEditUser.password
 	   $http.put("http://localhost:8080/api/user/"+$scope.currentEditUser.id,$scope.UserBody).then((res)=>{
+		$location.path('/users');
 		  $scope.Users=$scope.Users.map((ele)=>{
 			 return ele.id==res.data.id?res.data:ele;
 		  })
